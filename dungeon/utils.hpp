9858a1780bdb3f3ccf2585691b7bc889d1ff08b7
Zac Scheiwe <zacharyscheiwe@gmail.com>
2022-12-15 22:30:53 +1300

    Added deck of cards class Deck

---
+++ dungeon/utils.hpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int get_random_integer(const int& exclusive_max, const int& inclusive_min=0) {
+    return rand() % (exclusive_max - inclusive_min) + inclusive_min; 
+}
+
+template <typename T>
+void print_vector(T& vec) {
+    cout.precision(2);
+    for (int i=0; i<vec.size(); i++) {
+        cout << setw(3) << vec[i] << " ";
+    }
+    cout << "\n";
+}
+
+class Deck {
+    public:
+    vector<int> cards = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 9};
+
+    Deck() {
+        shuffle();
+    }
+
+    void shuffle() {
+        int c2, tmp;
+        for (int c1 = cards.size() - 1; c1 > 0; c1--) {
+            c2 = get_random_integer(c1 + 1);
+            tmp = cards[c1];
+            cards[c1] = cards[c2];
+            cards[c2] = tmp;
+        }
+    }
+
+    int draw() {
+        int out = cards.back();
+        cards.pop_back();
+        return out;
+    }
+
+    void print() {
+        print_vector(cards);
+    }
+};
+
+
